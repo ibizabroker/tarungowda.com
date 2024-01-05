b66bdd3ae32e94d2ac5af7b1e7ace9382873356c
tarun gowda <tarun.gowda2000@gmail.com>
2024-01-05 13:34:40 +0530

    theme switcher setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css"
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { Providers } from "@/components/providers"
+import { Providers } from "@/components/Providers"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html suppressHydrationWarning={true} lang="en">
       <body className={inter.className}>
         <Providers>
           {children}
