a4bd7ff07ba27bdfdf4526e9a2b1a48023555358
tarun gowda <tarun.gowda2000@gmail.com>
2024-02-12 23:18:36 +0530

    heading styles for markdown

--- src/components/Blog/MDXContent.tsx
+++ src/components/Blog/MDXContent.tsx
@@ -1,12 +1,21 @@
 import { useMDXComponent } from 'next-contentlayer/hooks'
 
+const components = {
+  h1: ({ ...props }) => <h2 className='h1-level' {...props} />,
+  h2: ({ ...props }) => <h2 className='h2-level' {...props} />,
+  h3: ({ ...props }) => <h3 className='h3-level' {...props} />,
+  h4: ({ ...props }) => <h4 className='h4-level' {...props} />,
+  h5: ({ ...props }) => <h5 className='h5-level' {...props} />,
+  h6: ({ ...props }) => <h2 className='h6-level' {...props} />
+};
+
 export default function MDXContent({ blog }: { blog: any }) {
 
   const MarkdownContent = useMDXComponent(blog.body.code)
 
   return (
     <div className='prose prose-lg max-w-max'>
-      <MarkdownContent />
+      <MarkdownContent components={components} />
     </div>
   )
 }
