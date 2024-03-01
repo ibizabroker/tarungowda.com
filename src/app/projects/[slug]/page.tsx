e2c34be834b56815524e251f96e9eaec6a7871ed
tarun gowda <tarun.gowda2000@gmail.com>
2024-03-01 22:44:26 +0530

    project page heading

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Projects, allProjects } from 'contentlayer/generated'
 import MDXComponent from '@/components/MDXContent'
 import NotFound from '@/app/not-found'
@@ -9,17 +10,52 @@ export default function ProjectPage({ params }: { params: Projects }) {
 
   return (
     <div className='container project-container'>
-      <div className='project-content'>
-        <main className='project-main'>
-          <div className='project-container'>
-            <article className='article'>
-              <div className='user-content'>
-                <MDXComponent mdx={project} />
-              </div>
-            </article>
-          </div>
-        </main>
+      <div className="flex items-start justify-between mb-4">
+        <h1 className="font-bold tracking-tight sm:text-5xl text-3xl mb-4 max-w-sm">
+          {project.title}
+        </h1>
+        <div className="flex justify-end">
+          {project.github? 
+            <a
+              href={project.github}
+              rel="noreferrer noopener"
+              target="_blank"
+              className="button-primary rounded-md px-4 py-2"
+            >
+              Github
+            </a>
+          :
+            <></>
+          }
+          {project.website? 
+            <a
+              href={project.website}
+              rel="noreferrer noopener"
+              target="_blank"
+              className="button-secondary rounded-md px-4 py-2 ml-4"
+            >
+              Website
+            </a>
+          :
+            <></>
+          }
+        </div>
       </div>
+
+      <div className="relative w-full h-40 pt-[52.5%]">
+        <Image
+          className="rounded-xl border dark:border-zinc-800 border-zinc-100 object-cover"
+          layout="fill"
+          src={project.coverImage}
+          alt={project.coverImage}
+          quality={100}
+        />
+      </div>
+      <article className='article'>
+        <div className='user-content'>
+          <MDXComponent mdx={project} />
+        </div>
+      </article>
     </div>
   )
 }
