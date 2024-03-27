5d2d3886de56446d4ba8986d47aef3be58c2050f
tarun gowda <tarun.gowda2000@gmail.com>
2024-03-28 00:50:31 +0530

    tentative intro text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,21 @@ function Home() {
   
   return (
     <div className="container m-10 mt-24 max-w-[1264px] mx-auto md:px-16 px-6">
-      <div className="mb-10">
-        <h1 className="max-w-3xl font-medium tracking-tight sm:text-5xl text-4xl mb-6 lg:leading-[3.7rem]">
-          Tarun Gowda
-        </h1>
-        <div className="relative w-full pt-[56.4%]">
-          <Image
-            className="rounded-xl border dark:border-divider-1 object-cover"
-            src="/static/projects/coverImage/ez-split.png"
-            alt="Tarun Gowda"
-            fill
-            style={{objectFit: "contain"}}
-            quality={100}
-          />
+      <div className="flex lg:flex-row flex-col xl:items-center items-start lg:justify-center justify-between gap-x-12 mb-10">
+        <div className="max-w-3xl">
+          <h1 className="font-medium tracking-tight sm:text-5xl text-4xl mb-6 lg:leading-[3.7rem]">
+            Hello! I'm Tarun.
+          </h1>
+          <p>Your everyday software developer exploring new tech to solve real-world problems. On this website I share some insights through technical blogs and projects that usually automates or solves the challenges I have faced in the past.</p>
         </div>
+        <Image
+          className="rounded-2xl"
+          src="/static/projects/coverImage/ez-split.png"
+          alt="Tarun Gowda"
+          width={570}
+          height={200}
+          quality={100}
+        />
       </div>
     </div>
   )
