563f693b1cc26305578c985d2245afcc7e853e2b
tarun gowda <tarun.gowda2000@gmail.com>
2024-04-02 18:29:53 +0530

    home styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,29 +9,29 @@ function Home() {
   const sortedProjects = sortProjectsUsingDate(allProjects);
   
   return (
-    <div className="container m-10 mt-24 max-w-[1264px] mx-auto md:px-16 px-6">
-      <div className="flex lg:flex-row flex-col xl:items-center items-start gap-x-12 gap-y-12 mb-10">
-        <div className="max-w-3xl">
-          <h1 className="font-medium tracking-tight sm:text-5xl text-4xl mb-6 lg:leading-[3.7rem]">
+    <div className="container home-container">
+      <div className="home-intro-container">
+        <div className="home-intro-main">
+          <h1 className="home-intro-heading">
             Hello! I'm Tarun.
           </h1>
           <p>Your everyday software developer exploring new tech to solve real-world problems. On this website I share some insights through technical blogs and projects that usually automates or solves the challenges I have faced in the past.</p>
         </div>
         <Image
-          className="rounded-2xl lg:w-[570px] w-full"
+          className="home-intro-image"
           src="/static/projects/coverImage/ez-split.png"
           alt="Tarun Gowda"
-          width={570}
-          height={200}
+          width={500}
+          height={100}
           quality={100}
         />
       </div>
-      <div className="my-2">
-        <h2 className="font-medium tracking-tight sm:text-4xl text-3xl mb-6 lg:leading-[3.7rem]">Recent Blogs</h2>
-        <BlogList sortedBlogs={sortedBlogs.slice(0, 3)} className="flex xl:w-[calc(100%_/3_-(20px/3*2))] md:w-[calc(100%_/2_-(20px/2*1))] w-full" />
+      <div className="home-recent-works">
+        <h2 className="home-recent-works-heading">Recent Blogs</h2>
+        <BlogList sortedBlogs={sortedBlogs.slice(0, 3)} className="home-blog-list" />
       </div>
-      <div className="mt-4">
-        <h2 className="font-medium tracking-tight sm:text-4xl text-2xl mb-6 lg:leading-[3.7rem]">Recent Projects</h2>
+      <div className="home-recent-works">
+        <h2 className="home-recent-works-heading">Recent Projects</h2>
         <ProjectList sortedProjects={sortedProjects.slice(0, 3)} />
       </div>
     </div>
